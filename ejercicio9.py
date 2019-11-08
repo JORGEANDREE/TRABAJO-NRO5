5b4be4213834bfcaa33b7ccce97151a902be9807
JORGEANDREE <57020796+jorgeandree@users.noreply.github.com>
2019-11-08 13:10:41 -0500

    se agrego el 9no ejercicio

---
+++ ejercicio9.py
@@ -0,0 +1,18 @@
+#calcular el area de un pentagono
+area,base,apotema=0.0,0,0
+
+#asignacion de valores
+base=57
+apotema=18
+
+#calculo
+area=(base*apotema)/2
+
+#mostrar valores
+print("la base es:",base)
+print("la apotema es:",apotema)
+print("el area es:",area)
+
+##verificador
+area_maxima=(area>200)
+print("el area es maxima:",area_maxima)
