623e7e97db3b29529978764325df4417c8b6a5cf
JORGEANDREE <57020796+jorgeandree@users.noreply.github.com>
2019-11-08 13:20:04 -0500

    se agrego el 12avo ejercicio

---
+++ ejercicio13.py
@@ -0,0 +1,18 @@
+#calcular el impulso
+impulso,fuerza,intervalo_tiempo=0.0,0.0,0.0
+
+#asignacion de valores
+fuerza=28
+intervalo_tiempo=12
+
+#calculo
+impulso=fuerza*intervalo_tiempo
+
+#mostrar valores
+print("la fuerza es:",fuerza)
+print("la intervalo_tiempo es:",intervalo_tiempo)
+print("el impulso es:",impulso)
+
+#verificador
+impulso_maximo=(impulso>400)
+print("el impulso es maximo:",impulso_maximo)
