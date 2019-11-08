e0e3983342b2327bac22d5c2f1dc5b856a04857e
JORGEANDREE <57020796+jorgeandree@users.noreply.github.com>
2019-11-08 13:35:30 -0500

    se agrego el 20avo ejercicio

---
+++ ejercicio20.py
@@ -0,0 +1,16 @@
+#calcular el volumen de un cubo
+arista,volumen=0,0.0
+
+#asignacion de valores
+arista=8
+
+#calculo
+volumen=arista*arista*arista
+
+#mostrar valores
+print("la arista:",arista)
+print("el volumen es:",volumen)
+
+#verificador
+volumen_maximo=(volumen>300)
+print("el volumen es maximo:",volumen_maximo)
