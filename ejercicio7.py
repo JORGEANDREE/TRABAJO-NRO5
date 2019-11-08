16f68f87d40e6d5c7c647c033944e50eee118eb6
JORGEANDREE <57020796+jorgeandree@users.noreply.github.com>
2019-11-08 13:07:42 -0500

    se agrego el 7to ejercicio

---
+++ ejercicio7.py
@@ -0,0 +1,18 @@
+#calcular el cantidad de movimiento
+movimiento,masa,velocidad=0.0,0.0,0.0
+
+#asignacion de valores
+masa=15.7
+velocidad=16
+
+#calculo
+movimiento=masa*velocidad
+
+#mostrar valores
+print("la masa es:",masa)
+print("la velocidad es:",velocidad)
+print("el movimiento es:",movimiento)
+
+#verificador
+movimiento_maximo=(movimiento>400)
+print("el movimiento es maximo:",movimiento_maximo)
