44f5482160c4027d4ae326c623e70742197673a0
JORGEANDREE <57020796+jorgeandree@users.noreply.github.com>
2019-11-08 12:52:30 -0500

    se agrego el 4to ejercicio

--- ejercicio4.py
+++ ejercicio4.py
@@ -1,20 +1,20 @@
 #calcular la posicion final
-posion_final,posion_inicial,velocidad,tiempo=0.0,0.0,0.0,0.0
+posicion_final,posicion_inicial,velocidad,tiempo=0.0,0.0,0.0,0.0
 
 #asignacion de valores
-posion_inicial=34.6
+posicion_inicial=34.6
 velocidad=20
 tiempo=12
 
 #calculo
-posion_final=posion_inicial+(velocidad*tiempo)
+posicion_final=posicion_inicial+(velocidad*tiempo)
 
 #mostrar valores
-print("la pocicion_inicial es:",posion_inicial)
+print("la posicion_inicial es:",posicion_inicial)
 print("la velocidad es:",velocidad)
 print("el tiempo es:",tiempo)
-print("la posicion_final es:",posion_final)
+print("la posicion_final es:",posicion_final)
 
 #verificador
-posicion_final_maxima=(posion_final>420)
+posicion_final_maxima=(posicion_final>420)
 print("la posicion final es maxima:",posicion_final_maxima)
