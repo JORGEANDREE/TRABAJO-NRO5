2a79481b34c55cee62a4087f61bc38aa9928e57a
JORGEANDREE <57020796+jorgeandree@users.noreply.github.com>
2019-11-08 13:11:58 -0500

    se agrego el 10mo ejercicio

---
+++ ejercicio10.py
@@ -0,0 +1,16 @@
+#calcular el area de un cuadrado
+area,lado=0,0
+
+#asignacion de valores
+lado=18
+
+#calculo
+area=lado*lado
+
+#mostrar valores
+print("el lado es:",lado)
+print("el area es:",area)
+
+##verificador
+area_maxima=(area>500)
+print("el area es maxima:",area_maxima)
