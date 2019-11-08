4a5a5f0cdce9218599711420533497048a277414
JORGEANDREE <57020796+jorgeandree@users.noreply.github.com>
2019-11-08 13:17:15 -0500

    se agrego el 11avo ejercicio

---
+++ ejercicio11.py
@@ -0,0 +1,16 @@
+#calcular el periodo(M.C.U)
+periodo,frecuencia=0.0,0.0
+
+#asignacion de valores
+frecuencia=12
+
+#calculo
+periodo=1/frecuencia
+
+#mostrar valores
+print("la frecuencia es:",frecuencia)
+print("el periodo es:",periodo)
+
+#verificador
+periodo_maximo=(periodo>400)
+print("el periodo es maximo:",periodo_maximo)
